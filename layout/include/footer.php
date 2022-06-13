6797ffc471a7c5cadda229802551810a3447328a
tunaytoksoz <tunaytoksoz@hotmail.com>
2022-06-14 01:02:10 +0300

    özet sayfası ve headerdaki route kısmı oluşturuldu.

--- layout/include/footer.php
+++ layout/include/footer.php
@@ -3,7 +3,7 @@
 <footer class="bg-light text-center text-lg-start">
     <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
         2022 ·
-        <a class="text-dark" href="#">Tunay</a>
+        <a class="text-dark" href="<?php echo "http://localhost/kisiselHarcama/" ?>">Tunay</a>
     </div>
 </footer>
 </body>
--- layout/include/header.php
+++ layout/include/header.php
@@ -2,13 +2,13 @@
 <header>
 <ul class="nav justify-content-center mb-5">
     <li class="nav-item">
-        <a class="nav-link active" aria-current="page" href="#spending">Harcama Ekle</a>
+        <a class="nav-link active" aria-current="page" href="<?php echo "http://localhost/kisiselHarcama/#spending" ?>">Harcama Ekle</a>
     </li>
     <li class="nav-item">
-        <a class="nav-link" href="#income">Gelir Ekle</a>
+        <a class="nav-link" href="<?php echo "http://localhost/kisiselHarcama/#income" ?>">Gelir Ekle</a>
     </li>
     <li class="nav-item">
-        <a class="nav-link" href="#">Özet</a>
+        <a class="nav-link" href="<?php echo "http://localhost/kisiselHarcama/summary.php" ?>">Özet</a>
     </li>
 </ul>
 </header>
